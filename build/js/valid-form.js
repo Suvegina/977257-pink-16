3ce32f7983e9de826ad710c1086b053e9896555b
Nadezhda <nadezhda@legeart.pro>
2019-05-27 22:32:03 +0300

    Внесены корректировки для блоков с декоративными элементами

---
+++ build/js/valid-form.js
@@ -0,0 +1,8 @@
+var form = document.querySelector("form");
+var surname = ["id=surname"];
+var name =  ["id=name"];
+var mail = ["id=mail"];
+
+form.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+  });
