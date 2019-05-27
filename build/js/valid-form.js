e9d29410556e80b7bc0d2942429ea1e5c33f63fa
Nadezhda <nadezhda@legeart.pro>
2019-05-27 22:38:13 +0300

    Удаление ошибочной папки build из гит

--- build/js/valid-form.js
+++
@@ -1,8 +0,0 @@
-var form = document.querySelector("form");
-var surname = ["id=surname"];
-var name =  ["id=name"];
-var mail = ["id=mail"];
-
-form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-  });
